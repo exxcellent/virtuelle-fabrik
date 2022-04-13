6cc1f4f2dd3f1860dcdeccb7a10060031c5214f2
NiklasThali <thalni@thu.de>
2022-04-13 16:51:56 +0200

    Erste Klassen implementiert

---
+++ Konzeption/Algorithmus/Product.py
@@ -0,0 +1,7 @@
+
+class Product(object):
+
+    def __init__(self, productID, sellPrice, receipeID):
+        self.productID = productID
+        self.sellPrice = sellPrice
+        self.receipeID = receipeID
---
+++ Konzeption/Algorithmus/Warehouse.py
@@ -0,0 +1,8 @@
+
+class Warehouse(object):
+
+    def __init__(self, stock, capacity, output, productID):
+        self.stock = stock
+        self.capacity = capacity
+        self.output = output
+        self.productID = productID

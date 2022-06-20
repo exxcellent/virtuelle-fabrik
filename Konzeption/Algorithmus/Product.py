c67039df97a9074d594fc24a3575c8777947e937
NiklasThali <thalni@thu.de>
2022-06-20 17:29:17 +0200

    DocStrings hinzugefügt

--- Konzeption/Algorithmus/Product.py
+++ Konzeption/Algorithmus/Product.py
@@ -1,24 +1,38 @@
-"""defines the product attributes
 
-:param product_ID: primary Key
-:type product_ID: int
-:param name: name of the product
-:type name: str
-:param sellPrice: selling price of product
-:type sellPrice: float
-:param recipe_ID: points to the recipe of the product
-:type recipe_ID: int
-"""
 class Product(object):
+    """defines the product attributes
+
+    :param product_ID: primary Key
+    :type product_ID: int
+    :param name: name of the product
+    :type name: str
+    :param sellPrice: selling price of product
+    :type sellPrice: float
+    :param recipe_ID: points to the recipe of the product
+    :type recipe_ID: int
+    """
     def __init__(self, product_ID: int, name: str, sellPrice: float, recipe_ID: int):
         self.product_ID = product_ID
         self.name = name
         self.sellPrice = sellPrice
         self.recipe_ID = recipe_ID
 
-# This class represents how much of each product is currently stored in the warehouse
+
 class ProductStorage(object):
     def __init__(self, stock: int, capacity: int, output: int, product_ID: int, costsPerUnit: float):
+        """This class represents how much of each product is currently stored in the warehouse
+
+        :param stock: stock count
+        :type stock: int
+        :param capacity: maximum capacity, which can be stored
+        :type capacity: int
+        :param output: product output per time unit due to shipping
+        :type output: int
+        :param product_ID: product ID which is stored
+        :type product_ID: int
+        :param costsPerUnit: storage costs of the product per time unit
+        :type costsPerUnit: float
+        """
         self.stock = stock
         self.capacity = capacity
         self.output = output
